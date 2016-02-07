beee3d46cb4e680f68be448952e945b13027c199
Sergiy <sergiy4947@yandex.ru>
2016-02-07 21:55:38 +0300

    ошибка

--- App/Models/User.php
+++ App/Models/User.php
@@ -7,12 +7,17 @@ use App\Db;
 
 class User
 {
+    const TABLE = 'users';
+
     public $email;
     public $name;
 
     public static function findAll()
     {
         $db = new Db();
-        return $db->query('SELECT * FROM users', 'App\Models\User');
+        return $db->query(
+            'SELECT * FROM' . self::TABLE,
+            self::class
+        );
     }
 } 
