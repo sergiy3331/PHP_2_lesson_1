8aca4dc8d96e37caa83767b7a52b15c505ae9041
Sergiy <sergiy4947@yandex.ru>
2016-02-08 21:29:15 +0300

    пробел после from

--- App/Models/User.php
+++ App/Models/User.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
-
 use App\Db;
 
 class User
 {
+    const TABLE = 'users';
+
     public $email;
     public $name;
 
     public static function findAll()
     {
         $db = new Db();
-        return $db->query('SELECT * FROM users', 'App\Models\User');
+        return $db->query('SELECT * FROM ' . self::TABLE, self::class);
     }
-} 
+}
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 require __DIR__ . '/autoload.php';
 
 $users = \App\Models\User::findAll();
+
 var_dump($users);
